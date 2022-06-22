7a49ad5f03e724c9a4483598631996e79d88c554
anniemon <seojung19@gmail.com>
2022-06-22 18:37:08 +0900

    update: jest.config.js

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,12 @@
 module.exports = {
   testEnvironment: 'node',
+  preset: 'ts-jest',
+  transform: { '^.+\\.(ts|tsx)$': 'ts-jest' },
   moduleNameMapper: {
     '@api/(.*)': '<rootDir>/build/api/$1',
     '@entity/(.*)': '<rootDir>/build/entity/$1',
     '@data-source': '<rootDir>/build/data-source/index.js',
+    '@lib/(.*)': '<rootDir>/build/lib/$1',
     '@server': '<rootDir>/build/server.js',
   },
 };
