11791a4e4f67eb1299fb68c60027341c8c48236d
anniemon <seojung19@gmail.com>
2022-06-23 18:13:43 +0900

    add: user login test, bulk insert when signup

--- src/api/v0/user/user.spec.ts
+++ src/api/v0/user/user.spec.ts
@@ -2,7 +2,7 @@ import { FastifyInstance } from 'fastify';
 import { build } from '@server';
 import { clearTablesAll } from '@lib/modules/raw-query';
 
-describe('test user signup', () => {
+describe('test user', () => {
   let server!: FastifyInstance;
 
   beforeAll(async () => {
@@ -13,22 +13,63 @@ describe('test user signup', () => {
     await server.close();
   });
 
-  try {
-    test('유저 회원가입', async () => {
-      const res = await server.inject({
-        method: 'POST',
-        url: '/api/v0/user/signup',
-        payload: {
-          type: 'SIGNUP',
-          name: 'user',
-          user_nickname: 'asdf',
-          user_email: 'asdf@gmail.com',
-          user_password: 'password',
-        },
-      });
-      expect(res.statusCode).toBe(200);
+  test('유저 회원가입', async () => {
+    try {
+      for (let i = 10000; i < 10010; i++) {
+        const res = await server.inject({
+          method: 'POST',
+          url: '/api/v0/user/signup',
+          payload: {
+            type: 'SIGNUP',
+            name: 'user',
+            user_nickname: `asdf${i}`,
+            user_email: `asdf${i}@gmail.com`,
+            user_password: 'password',
+          },
+        });
+        expect(res.statusCode).toBe(200);
+        expect(JSON.parse(res.body).data.id).toEqual(i);
+      }
+    } catch (err) {
+      console.error(err);
+    }
+  });
+
+  test('유저 로그인', async () => {
+    const resOk = await server.inject({
+      method: 'POST',
+      url: '/api/v0/user/login',
+      payload: {
+        type: 'LOGIN',
+        name: 'user',
+        user_email: 'asdf10000@gmail.com',
+        user_password: 'password',
+      },
     });
-  } catch (err) {
-    console.error(err);
-  }
+    expect(resOk.statusCode).toEqual(200);
+
+    const resWrongEmail = await server.inject({
+      method: 'POST',
+      url: '/api/v0/user/login',
+      payload: {
+        type: 'LOGIN',
+        name: 'user',
+        user_email: 'notnot@gmail.com',
+        user_password: 'password',
+      },
+    });
+    expect(resWrongEmail.statusCode).toEqual(500);
+
+    const resWrongPw = await server.inject({
+      method: 'POST',
+      url: '/api/v0/user/login',
+      payload: {
+        type: 'LOGIN',
+        name: 'user',
+        user_email: 'asdf10000@gmail.com',
+        user_password: 'wrongwrong',
+      },
+    });
+    expect(resWrongPw.statusCode).toEqual(500);
+  });
 });
