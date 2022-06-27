58bb71c698248416644a750a2d3e5c15132c9baf
anniemon <seojung19@gmail.com>
2022-06-27 23:43:39 +0900

    WIP: customize validate-access-token middleware

--- src/lib/middleware/validate-access-token.ts
+++ src/lib/middleware/validate-access-token.ts
@@ -1,20 +1,10 @@
 import { FastifyRequest, FastifyInstance, FastifyPluginOptions, FastifyPluginAsync } from 'fastify';
 import fp from 'fastify-plugin';
-// import { verifyAccessToken, closeOAuthCache } from '@lib/modules/';
 import { IncomingMessage } from 'http';
 
 export interface ServerIncomingMessage extends IncomingMessage {
   locals: object;
 }
-
-export interface validateRoutes {
-  includes: Array<string>;
-  excludes: Array<string>;
-}
-
-const routes = (options: FastifyPluginOptions, routeNames: Array<string>): Array<string> => {
-  return routeNames.map((route) => `${options.prefix}/${route}`);
-};
 
 const ACCESS_TOKEN_REGEXP = /^bearer\s+(.*)/i;
 /**
@@ -29,36 +19,27 @@ const getAccessToken = (req: FastifyRequest) => {
   return matched && matched[1];
 };
 
-export const validateAccessToken = async (selectedRoutes: validateRoutes) => {
+export const validateAccessToken = async () => {
   const validateTokenMiddie: FastifyPluginAsync = async (server: FastifyInstance, options: FastifyPluginOptions) => {
-    const includesRoutes = routes(options, selectedRoutes.includes);
-    const excludesRoutes = routes(options, selectedRoutes.excludes);
     server.addHook('preHandler', async (req) => {
       try {
-        if (excludesRoutes.some((routes) => (req.url as string).includes(routes))) {
-          if (!includesRoutes.some((routes) => (req.url as string).includes(routes))) {
-            return;
-          }
-        }
         const accessToken = getAccessToken(req);
         if (!accessToken) {
           throw new Error('invalid access token');
         }
-        const accessTokenCredential = JSON.stringify([req.headers['user-agent']]);
-        // const principal = await verifyAccessToken(accessToken, accessTokenCredential);
+        const verified = await server.jwt.verify(accessToken);
+        console.log(verified, 'verified');
+        console.log(accessToken, 'accessToken');
+
         (req.raw as ServerIncomingMessage).locals = {
           accessToken,
-          // principal,
         };
         //TODO: 사용자 세션 정보이다. route 에서는 이렇게 접근해야한다. access 함수를 만들어야 할것 같다.
-        //const locals = (request.raw as ServerIncomingMessage).locals;
+        //const locals = (request.raw as any).locals;
       } catch (error: unknown) {
         server?.log.error(error);
         throw error;
       }
-    });
-    server.addHook('onClose', async () => {
-      // await closeOAuthCache();
     });
   };
   return fp(validateTokenMiddie);
